279ab7338f101ba397122bb3d4f1face2e44de4d
coder_x <1019058432@qq.com>
2022-02-23 14:13:04 +0800

    加入serverWork install事件监听和fetch网络请求监听

--- sw.js
+++ sw.js
@@ -1,1 +1,44 @@
 console.log('service work');
+// 监听install事件
+this.addEventListener('install', event => {
+  event.waitUnitl(
+    caches.open(PWA_CACHE_TAG).then( cache => {
+      console.log('install over!');
+      return cache.addAll([
+        '/',
+        'index.html',
+        'main.css',
+        'main.js',
+        'image.jpg'
+      ])
+    })
+  )
+})
+// 监听网络请求事件
+this.addEventListener('fetch', event => {
+  event.respondWidth(
+    caches
+    .match(event.request)
+    .then( response => {
+      if (response) {
+        return response
+      }
+      console.log('has fetch event');
+      var request = event.request.clone()
+      return fetch(request)
+      .then( httpRes => {
+        if (!httpRes || httpRes != 200) {
+          return httpRes;
+        }
+        let responseClone = httpRes.clone();
+        caches
+        .open(PWA_CACHE_TAG)
+        .then( cache => {
+          cache.put(event.request, responseClone)
+        })
+
+        return httpRes
+      })
+    })
+  )
+})
